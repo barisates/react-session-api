82876abdc035d778ba7b6abe5510d64b5b1dfec8
bates <bates@trabilisim.com>
2019-10-08 17:47:21 +0300

    Fix SessionStorage Prototype

--- src/index.js
+++ src/index.js
@@ -1,128 +1,128 @@
 const Config = {
-    useBrowserStorage: false,
-    timeout: 0
+  useBrowserStorage: false,
+  timeout: 0
 }
 const Helper = {
-    access: new Date(),
-    expired: (storage) => {
+  access: new Date(),
+  expired: (storage) => {
 
-        const expireMinute = Math.round(((new Date() - Helper.access) / 60000));
+    const expireMinute = Math.round(((new Date() - Helper.access) / 60000));
 
-        if (expireMinute > Config.timeout && Config.timeout !== 0) {
-            console.warn("Session expired!", storage);
-            storage.clear();
-        }
-
-        Helper.access = new Date();
+    if (expireMinute > Config.timeout && Config.timeout !== 0) {
+      console.warn("Session expired!", storage);
+      storage.clear();
     }
+
+    Helper.access = new Date();
+  }
 }
 
 const Storage = () => {
-    try {
-        if (!Config.useBrowserStorage)
-            throw new Error();
+  try {
+    if (!Config.useBrowserStorage)
+      throw new Error();
 
-        const key = "jcfOnRWMIvigArtNb1z3hj6yQ2xlZGiD";
-        sessionStorage.setItem(key, key);
-        sessionStorage.removeItem(key);
+    const key = "jcfOnRWMIvigArtNb1z3hj6yQ2xlZGiD";
+    sessionStorage.setItem(key, key);
+    sessionStorage.removeItem(key);
 
-        return sessionStorage;
-    } catch (e) {
-        let SessionStorage = {}
+    return sessionStorage;
+  } catch (e) {
 
-        Object.defineProperty(SessionStorage, "length", {
-            get: function () { return Object.keys(this).length; },
-            enumerable: false,
-            __proto__: null
-        });
+    let SessionStorage = {}
 
-        SessionStorage.__proto__.setItem = (key, value) => {
-            SessionStorage[key] = value;
-        }
+    Object.defineProperty(SessionStorage, "length", {
+      get: function () { return Object.keys(this).length; },
+      enumerable: false,
+      __proto__: null
+    });
 
-        SessionStorage.__proto__.getItem = (key) => {
-            return SessionStorage[key];
-        }
+    Object.setPrototypeOf(SessionStorage, {
+      setItem: (key, value) => {
+        SessionStorage[key] = value;
+      },
+      getItem: (key) => {
+        return SessionStorage[key];
+      },
+      removeItem: (key) => {
+        delete SessionStorage[key];
+      },
+      clear: () => {
+        SessionStorage = {};
+      }
+    });
 
-        SessionStorage.__proto__.removeItem = (key) => {
-            delete SessionStorage[key];
-        }
-
-        SessionStorage.__proto__.clear = () => {
-            SessionStorage = {};
-        }
-
-        return SessionStorage;
-    }
+    return SessionStorage;
+  }
 }
 
 class SessionStorage {
 
-    constructor() {
-        this.Storage = Storage();
-        this.Callback = [];
-    }
-    /**
-     * @param {boolean} browserStorage Use browser sessionStorage.
-     * @param {number} timeout Session timeout period, in minutes.
-     */
-    config(browserStorage = false, timeout = 0) {
-        Config.useBrowserStorage = browserStorage;
-        Config.timeout = timeout;
-        this.Storage = Storage();
-    }
-    /**
-    * @return {Array<Object>} Return session items.
-    */
-    items() {
-        Helper.expired(this.Storage);
+  constructor() {
+    this.Storage = Storage();
+    this.Callback = [];
+  }
+  /**
+   * @param {boolean} browserStorage Use browser sessionStorage.
+   * @param {number} timeout Session timeout period, in minutes.
+   */
+  config(browserStorage = false, timeout = 0) {
+    Config.useBrowserStorage = browserStorage;
+    Config.timeout = timeout;
+    this.Storage = Storage();
+  }
+  /**
+  * @return {Array<Object>} Return session items.
+  */
+  items() {
+    Helper.expired(this.Storage);
 
-        let sessionData = {};
+    let sessionData = {};
 
-        Object.keys(this.Storage).forEach(item => { sessionData[item] = this.Storage.getItem(item) });
+    Object.keys(this.Storage).forEach(item => { sessionData[item] = this.Storage.getItem(item) });
 
-        return sessionData;
-    }
-    clear() {
-        this.Storage.clear();
-    }
-    /**
-     * @param {string} key Session item key.
-     * @param {Object|string} value Session item value. If you are using browser storage, it can only take {:string}.
-     */
-    set(key, value) {
-        Helper.expired(this.Storage);
+    return sessionData;
+  }
+  clear() {
+    this.Storage.clear();
+  }
+  /**
+   * @param {string} key Session item key.
+   * @param {Object|string} value Session item value. If you are using browser storage, it can only take {:string}.
+   */
+  set(key, value) {
+    Helper.expired(this.Storage);
 
-        this.Storage.setItem(key, value);
+    this.Storage.setItem(key, value);
 
-        const data = this.items();
+    const data = this.items();
 
-        this.Callback.forEach(func => {
-            func(data);
-        });
-    }
-    /**
-    * @param {string} key Session item key.
-    * @return {Object} Session item value. If you are using browser storage, it can return {:string}.
-    */
-    get(key) {
-        Helper.expired(this.Storage);
+    this.Callback.forEach(func => {
+      func(data);
+    });
+  }
+  /**
+  * @param {string} key Session item key.
+  * @return {Object} Session item value. If you are using browser storage, it can return {:string}.
+  */
+  get(key) {
+    Helper.expired(this.Storage);
 
-        return this.Storage.getItem(key);
-    }
-    /**
-    * @param {string} key Session item key.
-    */
-    remove(key) {
-        return this.Storage.removeItem(key);
-    }
-    /**
-    * @param {func} callback Triggered when session items set.
-    */
-    onSet(callback) {
-        callback(this.items())
-        this.Callback.push(callback);
-    }
+    return this.Storage.getItem(key);
+  }
+  /**
+  * @param {string} key Session item key.
+  */
+  remove(key) {
+    return this.Storage.removeItem(key);
+  }
+  /**
+  * @param {func} callback Triggered when session items set.
+  */
+  onSet(callback) {
+    callback(this.items())
+    this.Callback.push(callback);
+  }
 }
 
 const Session = new SessionStorage();
